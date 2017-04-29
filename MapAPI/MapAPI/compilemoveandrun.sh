b9b7bb3c423e4f8dc260af1f7e3bf5bab3f5d02e
jacobcheatley <jacobcheatley@gmail.com>
2017-04-29 14:33:06 +1200

    Actually ignore bash script

--- MapAPI/MapAPI/compilemoveandrun.sh
+++
@@ -1,8 +0,0 @@
-cd /home/cameron/Documents/MapAPI
-dotnet restore
-dotnet publish
-cd /home/cameron/Documents/MapAPI/bin/Debug/netcoreapp1.1/publish
-sudo rm -rf /var/www/*
-sudo cp -r ./* /var/www
-cd /var/www
-dotnet MapAPI.dll
